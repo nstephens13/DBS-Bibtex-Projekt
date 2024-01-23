f4dfed2037f37414433589d60f5f581803bc1730
lukas.ruminsk <lukas.ruminski@stud.htwk-leipzig.de>
2024-01-23 18:18:45 +0100

    first try to fix /bf

--- sourcecode/bibtoxml.py
+++ sourcecode/bibtoxml.py
@@ -5,10 +5,21 @@ import html
 
 def handle_formatting(text):
     """Helper function to handle BibTeX formatting commands"""
+    text = text.replace('{', '').replace('}', '')  # Remove curly braces
+    italic = False
+    bold = False
+
+    if '\\it' in text:
+        text = text.replace('\\it', '')
+        italic = True
+
     if '\\bf' in text:
-        return {'text': text.replace('\\bf', ''), 'style': 'bold'}
-    else:
-        return {'text': text}
+        text = text.replace('\\bf', '')
+        bold = True
+
+    return {'text': text, 'style': {'italic': italic, 'bold': bold}}
+
+
 
 def remove_curly_braces(text):
     """Helper function to remove curly braces from the text"""
@@ -26,8 +37,8 @@ def indent(elem, level=0):
             elem.text = i + "    "
         if not elem.tail or not elem.tail.strip():
             elem.tail = i
-        for elem in elem:
-            indent(elem, level + 1)
+        for child_elem in elem:
+            indent(child_elem, level + 1)
         if not elem.tail or not elem.tail.strip():
             elem.tail = i
     else:
@@ -46,22 +57,14 @@ def handle_special_characters(text):
         r'{\\aa}': 'å',
         r'{\\AA}': 'Å',
         r'{\\ss}': 'ß',
-        r'\\"a': 'ä',
-        r'\\"A': 'Ä',
-        r'\\"o': 'ö',
-        r'\\"O': 'Ö',
-        r'\\"u': 'ü',
-        r'\\"U': 'Ü',
+        r'\\"([a-zA-Z])': lambda match: match.group(1),
+        r'\\"{(.+?)}': lambda match: match.group(1).encode('latin-1').decode('unicode-escape'),
     }
 
     for pattern, replace_function in special_characters.items():
         text = re.sub(pattern, replace_function, text)
 
     return text
-
-
-
-
 
 def bibtex_to_xml(bibtex_file_path, output_file_path):
     with open(bibtex_file_path, 'r', encoding='utf-8') as bibtex_file:
@@ -75,16 +78,25 @@ def bibtex_to_xml(bibtex_file_path, output_file_path):
         entry_type = entry['ENTRYTYPE'].lower()
         entry_element = ET.SubElement(root, entry_type)
 
+
         for field, value in entry.items():
             if field != 'ENTRYTYPE':
                 field_element = ET.SubElement(entry_element, field.lower())
                 if field == 'volume':
                     formatted_text = handle_formatting(value)
                     field_element.text = formatted_text['text']
-                    if 'style' in formatted_text:
-                        field_element.set('style', formatted_text['style'])
-                elif field in ['title', 'booktitle']:
+                    styles = formatted_text['style']
+                    if styles['italic']:
+                        field_element.set('italic', 'true')
+                    if styles['bold']:
+                        field_element.set('bold', 'true')
+                elif field in ['title', 'booktitle', 'note']:
                     field_element.text = remove_curly_braces(handle_special_characters(value))
+                    styles = handle_formatting(value)['style']
+                    if styles['italic']:
+                        field_element.set('italic', 'true')
+                    if styles['bold']:
+                        field_element.set('bold', 'true')
                 elif field == 'editor':
                     field_element.text = handle_special_characters(value)
                 elif field == 'year':
